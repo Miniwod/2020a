29c2abfec4a70dec8bacf012de823eebfec5b4db
Miniwod <63182509+Miniwod@users.noreply.github.com>
2020-10-10 00:50:44 +0800

    Add files via upload

--- analy.java
+++ analy.java
@@ -1,6 +1,7 @@
 import java.io.*;
 
 public class analy {
+    public static char next=0;
     public static boolean isDigital(char c){
         if(c>='0' && c<='9') return true;
         else return false;
@@ -35,7 +36,7 @@ public class analy {
         boolean flag=false;
         String before="";
         String after="";
-        String[] tb={"BEGIN","END","IF","ELSE","THEN","+","*",":",",","(",")",":="};
+        String[] tb={"BEGIN","END","IF","ELSE","THEN","+","*",":=",":",",","(",")"};
         int l2=tb.length;
         int i;
         for (i=0;i<l2;i++){
@@ -71,104 +72,70 @@ public class analy {
         else print(tmps,0);
         return;
     }
+    public static boolean ckvalid(char c){
+        if((c>='0' && c<='9') || (c>='a' && c<='z') || (c>='A' && c<='Z') || c==':' || c=='+' || c=='*' || c==',' || c=='(' || c==')' || c=='=') return true;
+        return false;
+    }
     public static void main(String[] args){
         String p1="in.txt";
         String p2="/tests/1.in";
-        File file=new File(p2);
+        File file=new File(p1);
         Reader r=null;
         try {
             r=new InputStreamReader(new FileInputStream(file));
             int tmp;
             String tmps="";
             char ls=0;
-            boolean flag=false;
-//            while((tmp=r.read())!=-1){
-//                char c=(char) tmp;
-//                tmps+=c;
-//                if(tmps.equals(":")){
-//                    System.out.println("Colon");
-//                    flag=true;
-//                }
-//                else if(tmps.equals("+")){
-//                    System.out.println("Plus");
-//                    flag=true;
-//                }
-//                else if(tmps.equals("*")){
-//                    System.out.println("Star");
-//                    flag=true;
-//                }
-//                else if(tmps.equals(",")){
-//                    System.out.println("Comma");
-//                    flag=true;
-//                }
-//                else if(tmps.equals("(")){
-//                    System.out.println("LParenthesis");
-//                    flag=true;
-//                }
-//                else if(tmps.equals(")")){
-//                    System.out.println("RParenthesis");
-//                    flag=true;
-//                }
-//                else if(tmps.equals("BEGIN")){
-//                    System.out.println("Begin");
-//                    flag=true;
-//                }
-//                else if(tmps.equals("END")){
-//                    System.out.println("End");
-//                    flag=true;
-//                }
-//                else if(tmps.equals("FOR")){
-//                    System.out.println("For");
-//                    flag=true;
-//                }
-//                else if(tmps.equals("IF")){
-//                    System.out.println("If");
-//                    flag=true;
-//                }
-//                else if(tmps.equals("THEN")){
-//                    System.out.println("Then");
-//                    flag=true;
-//                }
-//                else if(tmps.equals("ELSE")){
-//                    System.out.println("Else");
-//                    flag=true;
-//                }
-//                else if(tmps.equals(":=")){
-//                    System.out.println("Assign");
-//                    flag=true;
-//                }
-//                else if(tmps.equals("pascal_lex")){
-//                    System.out.println("Ident(pascal_lex)");
-//                    flag=true;
-//                }
-//
-//                if(flag){
-//                    tmps="";
-//                    flag=false;
-//                }
-//            }
+            boolean flag=true;
             while((tmp=r.read())!=-1){
                 char c=(char) tmp;
-                if((c>='0' && c<='9') || (c>='a' && c<='z') || (c>='A' && c<='Z') || c==':' || c=='+' || c=='*' || c==',' || c=='(' || c==')' || c=='='){
+                if((c>='0' && c<='9') || (c>='a' && c<='z') || (c>='A' && c<='Z') || c==':' || c=='+' || c=='*' || c==',' || c=='(' || c==')'){
                     if(isDigital(ls) && !isDigital(c)){
                         if(isInt(tmps)) print(tmps,1);
                         else print(tmps,0);
                         tmps="";
                     }
                     tmps+=c;
-                    flag=check(tmps);
-                    if(flag) tmps="";
+                    if(c==':'){
+                        if((tmp=r.read())!=-1){
+                            next=(char) tmp;
+                            if(next=='='){
+                                tmps+=next;
+                                //System.out.println(tmps);
+                                check(tmps);
+                                tmps="";
+                            }
+                            else {
+                                check(tmps);
+                                if(ckvalid(next)) tmps=""+next;
+                                else if(next==' ' || next=='\n' || next=='\r') tmps="";
+                                else{
+                                    flag=false;
+                                    break;
+                                }
+                            }
+                        }
+                        else{
+                            check(tmps);
+                            break;
+                        }
+                    }
+                    else {
+                        flag=check(tmps);
+                        if(flag) tmps="";
+                    }
                 }
                 else if(c==' ' || c=='\n' || c=='\r'){
                     match(tmps);
                     tmps="";
                 }
                 else{
-                    System.out.println("Unknown");
+                    flag=false;
                     break;
                 }
                 ls=c;
             }
+            if(!flag) System.out.println("Unknown");
         }catch (Exception e) {
             e.printStackTrace();
         }
